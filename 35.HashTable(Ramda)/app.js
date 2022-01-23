dc9e1e271fb1ba7e631238b110c0ddbf7b263d09
Leebonggu <leebonggu12@hanmail.net>
2022-01-24 01:24:10 +0900

    35/add 결과리포트

--- 35.HashTable(Ramda)/app.js
+++ 35.HashTable(Ramda)/app.js
@@ -3,6 +3,7 @@ const { v4: uuidv4 } = require('uuid');
 const R = require('ramda');
 
 const result = {};
+const loop = 10000;
 const color = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const getUnicode = (str) => str.charCodeAt(0)
 const modulo = R.flip(R.modulo)
@@ -14,10 +15,23 @@ const getHashKey = R.pipe(
 )
 
 console.time('for');
-for (let i = 0 ; i <= 10000; i += 1) {
+for (let i = 0 ; i < loop; i += 1) {
   const uuid = uuidv4();
   const key = getHashKey(uuid)
   result[key] = result[key] ? result[key] + 1 : 1
 }
+
 console.timeEnd('for');
-console.log(result)
+
+const 평균 = loop / color.length
+const 편차제곱합 = Object.entries(result)
+  .map(([index, count]) => count)
+  .reduce((acc, cur) => {
+    const 편차 = cur - 평균;
+    return acc += (편차 * 편차)
+  } , 0)
+
+const 표준편차 = Math.sqrt(편차제곱합 / color.length);
+console.log('Loop', loop)
+console.log('결과', result)
+console.log('표준편차', 표준편차)
