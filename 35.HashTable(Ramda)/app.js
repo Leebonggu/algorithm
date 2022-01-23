fadba024639c3c6a521fd6f72a3adbd848a0b89e
Leebonggu <leebonggu12@hanmail.net>
2022-01-24 01:06:25 +0900

    35/HashTable

---
+++ 35.HashTable(Ramda)/app.js
@@ -0,0 +1,23 @@
+
+const { v4: uuidv4 } = require('uuid');
+const R = require('ramda');
+
+const result = {};
+const color = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const getUnicode = (str) => str.charCodeAt(0)
+const modulo = R.flip(R.modulo)
+const getHashKey = R.pipe(
+  R.split(''),
+  R.map(getUnicode),
+  R.reduce(R.add, 0),
+  modulo(color.length)
+)
+
+console.time('for');
+for (let i = 0 ; i <= 10000; i += 1) {
+  const uuid = uuidv4();
+  const key = getHashKey(uuid)
+  result[key] = result[key] ? result[key] + 1 : 1
+}
+console.timeEnd('for');
+console.log(result)
