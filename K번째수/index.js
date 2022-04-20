3d7cf9191f8a1f70420a54f21d5f710bf02e55b5
Leebonggu <leebonggu12@hanmail.net>
2022-04-20 14:39:17 +0900

    오름차순 알고리즘

--- K번째수/index.js
+++ K번째수/index.js
@@ -1,8 +1,27 @@
+// 정렬
+function sortAscending(arr) {
+  if (!arr.length) return [];
+
+  const copy = [...arr];
+
+  for (i = 0; i < copy.length; i += 1) {
+    for (j = i + 1; j < copy.length; j += 1) {
+      if (copy[i] > copy[j]) {
+        let temp = copy[i]
+        copy[i] = copy[j]
+        copy[j] = temp
+      }
+    }
+  }
+
+  return copy;
+}
+
 function solution(array, commands) {
-  return commands.map(c => 
-    array
-      .slice(c[0] - 1, c[1])
-      .sort((a, b) => a - b)[c[2] - 1]
+  return commands.map(command => {
+      const [ start, end, pick ] = command;
+      return  sortAscending(array.slice(start - 1, end))[pick - 1];
+    }
   )
 }
 
